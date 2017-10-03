e688ed690091fe048e772f7572b5d17057586e98
nussi98 <nussbaumer.domi@gmail.com>
2017-10-03 10:58:46 +0200

    compiled style.css

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-  gulp.src('css/**/*.scss')
+  gulp.src('../css/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('../css/'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('css/**/*.scss',['styles']);
+    gulp.watch('../css/**/*.scss',['styles']);
 });
