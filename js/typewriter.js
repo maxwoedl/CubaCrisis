2f99577d840df1d5802f33f6bc18f7e1cea8d8db
Maximilian Wödl <Max@Max-MacBook-Pro.local>
2017-11-07 12:01:49 +0100

    fixed typewriter function

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,17 +1,14 @@
 function typeWriter(text, n) {
   if (n < (text.length)) {
-    $('.contract').html(text.substring(0, n+1));
-    n++;
+    $('.contract').html(text.substring(0, n+1))
+    n++
     setTimeout(function() {
       typeWriter(text, n)
-    }, 20);
+    }, 20)
   }
 }
 
-$('.start').click(function(e) {
-  e.stopPropagation();
-
-  var text = $('.contract').data('text');
-
-  typeWriter(text, 0);
-});
+function startTyping() {
+  let text = $('.contract').data('text')
+  typeWriter(text, 0)
+}
