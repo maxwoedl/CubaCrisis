16953d6a31bfce372282b4ada530953460e92779
Maximilian Wödl <Max@Max-MacBook-Pro.local>
2017-11-28 12:42:12 +0100

    Added IntroJS

--- js/initTour.js
+++ js/initTour.js
@@ -12,7 +12,27 @@ for(let i = 0; i<stories.length;i++){
 }
 
 function startTour(){
-let body = document.getElementsByTagName("BODY")[0];
-body.classList.remove("preview");
-$("html, body,#ccc").animate({ scrollTop: 0 }, 600);
+  let body = document.getElementsByTagName("BODY")[0];
+  body.classList.remove("preview");
+  $("html, body,#ccc").animate({ scrollTop: 0 }, 600, function() {
+    // setTimeout(function() {startIntroJS();}, 2000);
+    startIntroJS()
+  });
+
+}
+
+function startIntroJS() {
+  introJs()
+    .setOption('showStepNumbers', false)
+    .setOption('showBullets', false)
+    .setOption('hideNext', true)
+    .setOption('hidePrev', true)
+    .setOption('disableInteraction', true)
+    .setOption('nextLabel', 'Weiter')
+    .setOption('prevLabel', 'Zurück')
+    .setOption('skipLabel', 'Überspringen')
+    .setOption('doneLabel', 'Fertig')
+    .setOption('keyboardNavigation', true)
+    .setOption('scrollToElement', false)
+    .start();
 }
