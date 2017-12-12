fde723270c0a8f0fb8700368f20918b085bd6f4c
BlubSta <nicolas.haller@hotmail.com>
2017-12-12 11:48:27 +0100

    Cookies
    Intro/Tutorial wird nur alle 7 Tage angezeigt

--- js/initTour.js
+++ js/initTour.js
@@ -14,8 +14,7 @@ zurueck.addEventListener('click', function(){
 function startTour(){
   let body = document.getElementsByTagName("BODY")[0];
   body.classList.remove("preview");
-  doEverything();
-  $("html, body,#ccc").animate({ scrollTop: 0 }, 600, function() {
+    $("html, body,#ccc").animate({ scrollTop: 0 }, 600, function() {
     // setTimeout(function() {startIntroJS();}, 2000);
     startIntroJS()
   });
@@ -23,7 +22,8 @@ function startTour(){
 }
 
 function startIntroJS() {
-  introJs()
+	checkCookie();
+ /* introJs()
     .setOption('showStepNumbers', false)
     .setOption('showBullets', false)
     .setOption('hideNext', true)
@@ -35,5 +35,47 @@ function startIntroJS() {
     .setOption('doneLabel', 'Fertig')
     .setOption('keyboardNavigation', true)
     .setOption('scrollToElement', false)
-    .start();
+    .start();*/
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var visited = getCookie("visited");
+    if (visited == "") {
+        setCookie("visited", "true", 7);
+        introJs()
+		    .setOption('showStepNumbers', false)
+			.setOption('showBullets', false)
+		    .setOption('hideNext', true)
+		    .setOption('hidePrev', true)
+		    .setOption('disableInteraction', true)
+		    .setOption('nextLabel', 'Weiter')
+		    .setOption('prevLabel', 'Zurück')
+		    .setOption('skipLabel', 'Überspringen')
+		    .setOption('doneLabel', 'Fertig')
+		    .setOption('keyboardNavigation', true)
+		    .setOption('scrollToElement', false)
+		    .start();
+    }    
 }
